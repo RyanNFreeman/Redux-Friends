19dcc3208e4baf22eb8e35f61cd4b87cf641149a
RyanNFreeman <ryanfreeman@Ryans-iMac.local>
2019-03-07 14:43:34 -0700

    added Route and Link

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,26 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
 import './App.css';
+import Login from './components/Login'
+import Home from './components/Home'
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <NavLink to='/friends'>Home</NavLink>
+          <NavLink to='/login'>My Friends</NavLink>
         </header>
+        <Route path='/friends' component={Home} />
+        <Route path='/login' component={Login}/>
       </div>
+      </Router>
     );
   }
 }
---
+++ friends/src/components/Login.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class Login extends React.Component {
+    state = {
+
+    }
+
+    render() {
+        return (
+            <form>
+                <li>Username</li>
+                <li>Password</li>
+                <submit></submit>
+            </form>
+        )
+    }
+}
+
+export default Login;
